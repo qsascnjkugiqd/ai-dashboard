e0e55de39e3d6d35f8fc1205a346a246cb4a9efd
qianxinyu.qian(錢心宇-WTD) <MTA70@waffer.com>
2025-11-19 15:26:31 +0800

    chore: set vite base for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { semiTheming } from "vite-plugin-semi-theming";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
+    base: '/ai-dashboard/',
     plugins: [
         react(),
         semiTheming({
